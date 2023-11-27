e7cbec8a2e7957fde2865b3fe8c3e813496509fb
Ayham Hammami <ayhmhamame14@gmail.com>
2023-11-28 00:17:37 +0300

    feat (api/apiHooks) : add some new logic to the useRefreshToken hook

--- src/api/apiHooks/useRefreshToken.ts
+++ src/api/apiHooks/useRefreshToken.ts
@@ -1,4 +1,6 @@
+import { CustomEndPoints } from "api/constants/customEndPoints";
 import { showErrorMessage } from "api/helpers/showErrorMessage";
+import { AxiosError } from "axios";
 import useHandleUserCredantilesInStorge from "features/auth/hooks/useHandleUserCredantilesInStorge";
 import { AuthSliceActions } from "features/auth/redux/slices/authSlice";
 import {
@@ -7,6 +9,8 @@ import {
 } from "features/common/hooks/useReduxHooks";
 import { Axios } from "libs/axios";
 import { useNavigate } from "react-router-dom";
+import { PagesRotes } from "router/constants/pagesRoutes";
+import { AuthServiceName } from "services/authService";
 
 export const useRefreshToken = () => {
   const navigate = useNavigate();
@@ -17,7 +21,7 @@ export const useRefreshToken = () => {
     const res = await Axios({
       method: "POST",
       //need service name
-      url: `/token-refresh`,
+      url: `${AuthServiceName}/${CustomEndPoints.Refresh}`,
       data: {
         refresh: tokens.refreshToken,
       },
@@ -25,19 +29,24 @@ export const useRefreshToken = () => {
       .then((res) => {
         return res;
       })
-      .catch((err) => {
-        console.log(err);
+      .catch((err: AxiosError) => {
         if (err) {
-          showErrorMessage(err);
+          showErrorMessage(err.message);
+          navigate(PagesRotes.AuthRoutes.login);
           removeUserCredantilesInStorge();
           dispatch(AuthSliceActions.Logout());
-          navigate("/auth/formLogin");
         }
       });
     if (res) {
-      console.log(res);
-      dispatch(AuthSliceActions.SetTokens(res.data.tokens));
-      return res.data;
+      const tokens = res.data.data;
+      console.log(tokens);
+      dispatch(
+        AuthSliceActions.SetTokens({
+          accessToken: tokens.accessToken,
+          refreshToken: tokens.refreshToken,
+        })
+      );
+      return tokens;
     }
   };
   return { getNewTokens };
