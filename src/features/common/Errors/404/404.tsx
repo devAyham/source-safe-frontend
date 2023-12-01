714354742e5aa18129895bed3209affe7f61acf4
Ayham Hammami <ayhmhamame14@gmail.com>
2023-12-01 19:31:34 +0300

    feat (features/common/Errors) : add new 404 error page

--- src/features/common/Errors/404/404.tsx
+++ src/features/common/Errors/404/404.tsx
@@ -1,19 +1,30 @@
-import { useTranslation } from "react-i18next";
-import styles from "../styles.module.scss";
-import { ReactComponent as SVG404 } from "assets/svgs/404_svg.svg";
+import { Result } from "antd";
+import { Button } from "components";
+import styles from "./styles.module.scss";
+import { useNavigate } from "react-router-dom";
+import { MainFeaturesRoutes } from "router/constants/mainFeaturesRoutes";
 /**
  * @description a component that used to show an ui error intereface for 404 errors
  */
 const FourOFourPage = () => {
-  const { t } = useTranslation();
+  const navigate = useNavigate();
   return (
-    <div className={styles.errorPage}>
-      <div className={styles.container}>
-        <SVG404 className={styles.image} />
-        <div className={styles.text}>
-          {t("LOOKS_LIKE_THE_PAGE_YOU_ARE_LOOKING_FOR_DOES_NOT_EXIST")}
-        </div>
-      </div>
+    <div className={styles.container}>
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={
+          <Button
+            onClick={() => {
+              navigate(MainFeaturesRoutes.DashboardRoute);
+            }}
+            type="primary"
+          >
+            Back Home
+          </Button>
+        }
+      />
     </div>
   );
 };
