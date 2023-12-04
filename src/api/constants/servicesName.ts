7294bb37fc3239f5abe0219bb604599a8c3360f5
Ayham Hammami <ayhmhamame14@gmail.com>
2023-12-04 22:47:38 +0300

    feat (src/api/constants) : add new service name for folder serivce

--- src/api/constants/servicesName.ts
+++ src/api/constants/servicesName.ts
@@ -1,9 +1,10 @@
 // Define an enum with the possible service types
 export enum ServiceType {
   Auth = "auth",
+  Folder = "folder",
 }
 // Define an array of service names as a readonly tuple
-const ServicesName = ["AuthService"] as const;
+const ServicesName = ["AuthService", "FolderService"] as const;
 // Define a type alias for the union of all service names
 
 type ServicesName = (typeof ServicesName)[number]; // type ServicesName = "CitiesService"|"RegionsService"
@@ -11,4 +12,5 @@ type ServicesName = (typeof ServicesName)[number]; // type ServicesName = "Citie
 // Define an object that maps each service name to its corresponding type
 export const ServicesTypes: Record<ServicesName, ServiceType> = {
   AuthService: ServiceType.Auth,
+  FolderService: ServiceType.Folder,
 } as const;
