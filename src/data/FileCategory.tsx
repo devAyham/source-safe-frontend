c6d3c89b4c1b9da367bc667054cce34d24fabfbd
Ayham Hammami <ayhmhamame14@gmail.com>
2023-12-04 21:54:29 +0300

    feat (src/data) : add files category for the files types

---
+++ src/data/FileCategory.tsx
@@ -0,0 +1,39 @@
+import {
+    faClapperboard,
+    faFileCircleQuestion,
+    faFileLines,
+    faImage,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { ReactNode } from "react";
+import variables from "styles/variables/_main_colors_vars.module.scss";
+import { FileTypesType } from "types/FilesTypes.type";
+
+export const fileCategory: {
+  [key in FileTypesType]: {
+    icon: ReactNode;
+    color: string;
+    text: string;
+  };
+} = {
+  video: {
+    text: "Videos",
+    icon: <FontAwesomeIcon icon={faClapperboard} />,
+    color: variables.primary_color_one,
+  },
+  image: {
+    icon: <FontAwesomeIcon icon={faImage} />,
+    color: variables.success_dark,
+    text: "Images",
+  },
+  document: {
+    icon: <FontAwesomeIcon icon={faFileLines} />,
+    color: variables.warninig_dark,
+    text: "Ducuments",
+  },
+  other: {
+    icon: <FontAwesomeIcon icon={faFileCircleQuestion} />,
+    color: variables.secondary_color_one,
+    text: "Others",
+  },
+};
