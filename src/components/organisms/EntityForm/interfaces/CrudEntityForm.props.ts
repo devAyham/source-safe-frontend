5772b795d7277529762690894ac0c0d1dfd5c5fe
Ayham Hammami <ayhmhamame14@gmail.com>
2023-12-07 10:25:04 +0300

    ref (components/organisms/EntityForm) : refactor the Entity form to have onSubmit callBack

--- src/components/organisms/EntityForm/interfaces/CrudEntityForm.props.ts
+++ src/components/organisms/EntityForm/interfaces/CrudEntityForm.props.ts
@@ -1,78 +1,79 @@
-import {ServiceType} from "../../../../api/constants/servicesName";
-import {EntityIdType} from "../../../../types/EntityId.type";
-import {MutationOptions} from "react-query";
-import {ErrorResponse} from "../../../../api/apiHooks/useHandleResponse";
-import {EntityFormProps} from "./EntityForm.props";
-import {GenericOmit} from "types/GenericOmit.type";
-import {ICrudResponse} from "api/services/apiCrud";
-import {EntityIdInterface} from "../../../../interfaces/EntityId.interface";
-import {IRequestParams} from "../../../../api/interfaces/requestParams";
+import { ServiceType } from "../../../../api/constants/servicesName";
+import { EntityIdType } from "../../../../types/EntityId.type";
+import { MutationOptions } from "react-query";
+import { ErrorResponse } from "../../../../api/apiHooks/useHandleResponse";
+import { EntityFormProps } from "./EntityForm.props";
+import { GenericOmit } from "types/GenericOmit.type";
+import { ICrudResponse } from "api/services/apiCrud";
+import { EntityIdInterface } from "../../../../interfaces/EntityId.interface";
+import { IRequestParams } from "../../../../api/interfaces/requestParams";
 
 export interface EntityFormContainerProps<
-    SubmittedValues,
-    createRequest,
-    patchRequest,
-    getResponse,
-    extraData
+  SubmittedValues,
+  createRequest,
+  patchRequest,
+  getResponse,
+  extraData
 > extends GenericOmit<
     EntityFormProps<
-        SubmittedValues,
-        createRequest,
-        patchRequest,
-        getResponse,
-        extraData
+      SubmittedValues,
+      createRequest,
+      patchRequest,
+      getResponse,
+      extraData
     >,
     "onSubmit" | "initialValues"
-> {
-    entityId?: EntityIdType;
-    serviceName: ServiceType;
-    extraData?: extraData;
-    apiOptions?: {
-
-        // Configuration for the createEntity request
-        createConfig?: MutationOptions<
-            ICrudResponse<getResponse>,
-            ErrorResponse,
-            createRequest | SubmittedValues
-        > & {
-            withOutFeedBackMessage?: boolean;
-        };
-        // Configuration for the patchEntity request
-        patchConfig?: MutationOptions<
-            ICrudResponse<getResponse>,
-            ErrorResponse,
-            Partial<patchRequest | SubmittedValues>
-        > & {
-            withOutFeedBackMessage?: boolean;
-        };
-        // Configuration for the getDetailsEntity request
-        getDetailsConfig?: {
-            enabled?: boolean;
-
-            // Optional callback function to execute on successful getDetails request
-            onSuccess?: (
-                data: ICrudResponse<getResponse>,
-                params: unknown,
-                meta: unknown,
-                extra?: unknown
-            ) => void;
-            // Optional callback function to execute on error in getDetails request
-            onError?: (error: ErrorResponse, params: unknown, meta: unknown) => void;
-            // Optional flag to disable feedback messages on error
-            withOutFeedBackMessage?: boolean;
-        }
+  > {
+  handleSubmit?: (
+    values: SubmittedValues & extraData,
+    extraData?: extraData
+  ) => any;
+  entityId?: EntityIdType;
+  serviceName: ServiceType;
+  extraData?: extraData;
+  apiOptions?: {
+    // Configuration for the createEntity request
+    createConfig?: MutationOptions<
+      ICrudResponse<getResponse>,
+      ErrorResponse,
+      createRequest | SubmittedValues
+    > & {
+      withOutFeedBackMessage?: boolean;
     };
-    mappers?: {
-        mapCreateEntity?: (
-            data: SubmittedValues & extraData
-        ) => createRequest | false;
-        mapPatchEntity?: (
-            data: SubmittedValues & extraData & { id: EntityIdType }
-        ) => patchRequest | false;
-        mapGetDetailsEntity?: (
-            data: getResponse
-        ) => Partial<
-            Record<keyof (SubmittedValues & extraData), any>
-        >;
+    // Configuration for the patchEntity request
+    patchConfig?: MutationOptions<
+      ICrudResponse<getResponse>,
+      ErrorResponse,
+      Partial<patchRequest | SubmittedValues>
+    > & {
+      withOutFeedBackMessage?: boolean;
     };
+    // Configuration for the getDetailsEntity request
+    getDetailsConfig?: {
+      enabled?: boolean;
+
+      // Optional callback function to execute on successful getDetails request
+      onSuccess?: (
+        data: ICrudResponse<getResponse>,
+        params: unknown,
+        meta: unknown,
+        extra?: unknown
+      ) => void;
+      // Optional callback function to execute on error in getDetails request
+      onError?: (error: ErrorResponse, params: unknown, meta: unknown) => void;
+      // Optional flag to disable feedback messages on error
+      withOutFeedBackMessage?: boolean;
+    };
+  };
+  mappers?: {
+    mapCreateEntity?: (
+      data: SubmittedValues & extraData
+    ) => createRequest | false;
+    mapPatchEntity?: (
+      data: SubmittedValues & extraData & { id: EntityIdType }
+    ) => patchRequest | false;
+    mapGetDetailsEntity?: (
+      data: getResponse
+    ) => Partial<Record<keyof (SubmittedValues & extraData), any>>;
+  };
 }
