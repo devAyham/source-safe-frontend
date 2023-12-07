eda41a51f9fbf328d2789135fb24482d5e9bcef7
Ayham Hammami <ayhmhamame14@gmail.com>
2023-12-07 10:18:46 +0300

    feat (src/api/intefaces) : edit the main response interface to suit the backend response shape

--- src/api/interfaces/baseResponse.ts
+++ src/api/interfaces/baseResponse.ts
@@ -1,26 +1,20 @@
-import {UseMutationOptions, UseQueryOptions} from "react-query";
-import {IPaginationConfig} from "./paginationConfig";
+import { UseMutationOptions, UseQueryOptions } from "react-query";
+import { IPaginationConfig } from "./paginationConfig";
 
-export interface CrudTableResponseData<T> {
-    items: T[]
-    pagination: IPaginationConfig
-}
+// export interface CrudTableResponseData<T> {
+//     items: T[]
+//     pagination: IPaginationConfig
+// }
 
 export interface IBaseApiResponse<T> {
-    data: CrudTableResponseData<T>
-    meta: any
-    extra: any
+  data: T[];
+  meta: any;
+  extra: any;
 }
-
 
 export interface IUseQueryOptions
-    extends Omit<UseQueryOptions<any, any, any, any>, "queryKey" | "queryFn"> {
-}
+  extends Omit<UseQueryOptions<any, any, any, any>, "queryKey" | "queryFn"> {}
 
 // @ts-ignore
 export interface IUseMutationOptions
-    extends Omit<UseMutationOptions<any, any, any, any>, "mutationFn"> {
-}
-
-
-
+  extends Omit<UseMutationOptions<any, any, any, any>, "mutationFn"> {}
