27cb7277cbb11f3a8e9e69e6e8ae098518ec75b3
Ayham Hammami <ayhmhamame14@gmail.com>
2023-12-07 13:44:37 +0300

    feat (src/features/auth/component/Modals) : edit the text for the logout modal

--- src/features/auth/components/Modals/Logout.tsx
+++ src/features/auth/components/Modals/Logout.tsx
@@ -1,8 +1,6 @@
-import { useState } from "react";
 import { Modal } from "antd";
-import { useTranslation } from "react-i18next";
-import { Button } from "components";
 import { useLogout } from "features/auth/apis/useLogout";
+import { useState } from "react";
 import { LogoutMenuItem } from "../LogoutMenuItem";
 /**
  *
@@ -24,13 +22,13 @@ const Logout = () => {
       <Modal
         open={open}
         onOk={onOk}
-        title={"ARE_YOU_SURE_YOU_WANT_TO_LOGOUT"}
+        title={"Are you sure you want to logout"}
         closable={false}
         onCancel={onCancel}
         okButtonProps={{ danger: true }}
         cancelButtonProps={{ danger: true }}
-        okText={"LOGOUT"}
-        cancelText={"CANCEL"}
+        okText={"Logout"}
+        cancelText={"Cancel"}
         confirmLoading={isLoading}
         width={310}
       />
