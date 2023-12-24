0eef6c6fe5942dc7b846f653c1155dd98902fd50
Ayham Hammami <ayhmhamame14@gmail.com>
2023-12-24 17:54:30 +0300

    ref (src/services/folderService/interfaces) : add the members to the entity and remove it from the get resposne interface

--- src/services/folderService/interfaces/Entity.interface.ts
+++ src/services/folderService/interfaces/Entity.interface.ts
@@ -1,7 +1,9 @@
 import { EntityIdInterface } from "../../../interfaces/EntityId.interface";
+import { MemberType } from "../types/member.type";
 
 export default interface IEntity extends EntityIdInterface {
   logo: any;
   name: string;
   created_at?: string;
+  members: MemberType[];
 }
--- src/services/folderService/interfaces/GetResponse.interface.ts
+++ src/services/folderService/interfaces/GetResponse.interface.ts
@@ -2,6 +2,5 @@ import { MemberType } from "../types/member.type";
 import IEntity from "./Entity.interface";
 
 export default interface IGetResponse extends IEntity {
-  members: MemberType[];
   files: string[];
 }
