fbe12ca9f2f45584bfbf1e435787fe8c38116c67
Ayham Hammami <ayhmhamame14@gmail.com>
2023-12-24 18:17:33 +0300

    feat (src/components/particels/ContentInfo/statisticsSection) : add new fake data

--- src/components/particels/ContentInfo/Contents/FolderInfoContent/components/StatisticsSection/Component.tsx
+++ src/components/particels/ContentInfo/Contents/FolderInfoContent/components/StatisticsSection/Component.tsx
@@ -6,6 +6,7 @@ function Component() {
     { filesCount: 400, fileType: "video", size: 40 },
     { filesCount: 1300, fileType: "image", size: 50 },
     { filesCount: 660, fileType: "document", size: 77 },
+    { filesCount: 660, fileType: "audio", size: 77 },
     { filesCount: 100, fileType: "other", size: 10 },
   ];
   return (
