3f4ea14182e2c3e3730625c2dbe789565d85917c
Ayham Hammami <ayhmhamame14@gmail.com>
2023-12-27 00:25:25 +0300

    feat (src/helpers) : add new helper to convert size for files

---
+++ src/helpers/convertFileSize.ts
@@ -0,0 +1,13 @@
+export function convertFileSize(
+  fileSizeInBytes: number,
+  unitOption: "GB" | "MB" | "KB"
+): string {
+  const units: Record<string, number> = {
+    GB: 1024 * 1024 * 1024,
+    MB: 1024 * 1024,
+    KB: 1024,
+  };
+
+  const convertedSize = fileSizeInBytes / units[unitOption];
+  return `${convertedSize.toFixed(2)} ${unitOption}`;
+}
