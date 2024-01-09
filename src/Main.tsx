055e1031b97b17f7681d96c1b3313edba7365527
Ayham Hammami <ayhmhamame14@gmail.com>
2024-01-09 20:08:39 +0300

    src (src) : comment the notification controller from the Main.tsx

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,7 +1,7 @@
 import { ReactQueryDevtools } from "react-query/devtools";
 import Router from "./router/Router";
 import CustomSuspence from "features/common/layouts/Suspence/Suspence";
-import NotificationsController from "features/notifications/controllers/NotificationsController";
+// import NotificationsController from "features/notifications/controllers/NotificationsController";
 // import FirebaseProvider from "providers/FirebaseProvider/FirebaseProvider";
 import AuthProvider from "providers/AuthProvider/AuthProvider";
 import AntDesignConfigProvider from "providers/AntDesignConfigProvider/AntDesignConfigProvider";
@@ -26,9 +26,9 @@ function Main() {
           {/* <FirebaseProvider> */}
           <AuthProvider>
             <CustomSuspence>
-              <NotificationsController>
+              {/* <NotificationsController> */}
                 <Router />
-              </NotificationsController>
+              {/* </NotificationsController> */}
             </CustomSuspence>
           </AuthProvider>
           {/* </FirebaseProvider> */}
